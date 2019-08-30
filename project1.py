83f5e9d5ba46eeb7a89b92f71e990f622ff0a26c
kmaasrud <54394333+kmaasrud@users.noreply.github.com>
2019-08-30 13:02:21 +0200

    defined tentative tdma function in python

--- project1.py
+++ project1.py
@@ -9,7 +9,10 @@ def tdma(a,b,c,d):
     c_tilde[0]=c[0]/b[0]
     d_tilde[0]=d[0]/b[0]
 
-    for i in range(1,n-2):
+    for i in range(1,n-1):
         m=1.0/(b[i]-a[i]*c_tilde[i-1])
         c_tilde[i]=c[i]*m
         d_tilde[i]=(d[i]-a[i]*d_tilde[i-1])*m
+
+    for i in reversed(range(1,n-1)):
+        x[i]=d_tilde[i]-c_tilde[i]+d[i+1]
