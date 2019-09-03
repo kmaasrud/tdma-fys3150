7609ca4318e030da795d503098db65b75a18c090
kmaasrud <54394333+kmaasrud@users.noreply.github.com>
2019-09-03 17:48:28 +0200

    Working program, but the TDMA-function gives a wrong solution.

--- Code/Python/tdma_function.py
+++ Code/Python/tdma_function.py
@@ -1,7 +1,8 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-def tdma(a,b,c,d,x):
-    n=len(b)
+def tdma(a,b,c,d,u):
+    n=len(b)    #the final solution (u) is of size n+2 because of the boundary conditions
 
     c_tilde=np.zeros(n)
     d_tilde=np.zeros(n)
@@ -10,13 +11,39 @@ def tdma(a,b,c,d,x):
     d_tilde[0]=d[0]/b[0]
 
     for i in range(1,n-1):
-        m=1.0/(b[i]-a[i]*c_tilde[i-1])
+        m=1.0/(b[i]-a[i]*c_tilde[i-1])  #saves 1 division, 1 multiplication and 1 subtraction
         c_tilde[i]=c[i]*m
         d_tilde[i]=(d[i]-a[i]*d_tilde[i-1])*m
 
-    v=np.zeros(n)
+    v=np.zeros(n)   #this represents the vector v, the solution of the linear set of equations Av=d
 
     for i in reversed(range(1,n-1)):
         v[i]=d_tilde[i]-c_tilde[i]+d[i+1]
 
-    x[1:n]=v
+    u[1:n+1]=v  #inserting the solution of the linear eq. into the final solution, with boundary conditions
+
+n=[10,100,1000]
+
+for i in n:     #solving for different n's
+    h=1/(i+1)
+    a=np.zeros(i-1)
+    a[:]=-1
+    b=np.zeros(i)
+    b[:]=2
+    c=np.zeros(i-1)
+    c[:]=-1
+    d=np.linspace(0,1,i)
+    d=h**2*100*np.exp(-10*d)
+    u=np.zeros(i+2)
+    x=np.linspace(0,1,i+2)
+    u[1]=55
+
+    tdma(a,b,c,d,u)
+
+    plt.plot(x,u,label=r'$n=$ '+str(i))
+
+actual_u=1-(1-np.exp(-10))*x-np.exp(-10*x)  #the analytically found solution
+plt.plot(x,actual_u,label="Analytical")
+
+plt.legend()
+plt.show()
