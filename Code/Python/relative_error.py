8354812da8e68a14f6f7902ca4ba1d9c8bf68849
annasro <anna.stray.rongve@gmail.com>
2019-09-08 14:14:48 +0200

    idk

--- Code/Python/relative_error.py
+++ Code/Python/relative_error.py
@@ -11,7 +11,7 @@ def poisson_tdma(d,v):
     c_tilde[0]=-0.5
     d_tilde[0]=0.5*d[0]
 
-    for i in range(1,n-1):  #FORWARD substitution
+    for i in range(n):  #FORWARD substitution
         m=1.0/(2+c_tilde[i-1])
         c_tilde[i]=-m
         d_tilde[i]=(d[i]+d_tilde[i-1])*m
@@ -23,28 +23,37 @@ def poisson_tdma(d,v):
 
     v[1:n+1]=v_temp  #inserting the solution of the linear eq. into the final solution, with boundary conditions
 
-n=np.array([int(1e2),int(1e3),int(1e4),int(1e5),int(1e6)])
-h=1/(n+1)
+n = np.rint(np.logspace(1,7,15))
+#n = np.array([1e1,1e2,1e3,1e4,1e5,1e6])
 epsilon=np.zeros(len(n))
+h=1/(n+1)
 
+print(n)
 for i in range(len(n)):     #solving for different n's
     x=np.linspace(0,1,n[i]+2)
     d=h[i]**2*100*np.exp(-10*x[1:-1])
-    v=np.zeros(n[i]+2)
+    v=np.zeros(int(n[i]+2))
     v[1]=55
 
-    x=np.linspace(0,1,n[i]+2)
     actual_v=1-(1-np.exp(-10))*x-np.exp(-10*x)  #the analytically found solution
 
     poisson_tdma(d,v)
+<<<<<<< HEAD
 
     print(v.shape, v[-1], v[-2])
     epsilon[i]=np.log10(np.max(np.absolute((v[2:-1]-actual_v[2:-1])/actual_v[2:-1])))
+=======
+    epsilon[i]=np.log10(np.max(np.absolute((v[2:-1]-actual_v[2:-1])/actual_v[2:-1])))
+
+>>>>>>> fdad2cc0f4d22afb2a8c1a9aec40c1d987315276
 
 print("| Relative error  | log(Step size) |")
 print("|-----------------|----------------|")
 for i in range(len(epsilon)):
     print("| "+str(epsilon[i])+" | "+str(np.log10(h[i]))+" |")
 
+plt.title("Log10 of the error againts log10 of the step size")
+plt.xlabel("log10(h)")
+plt.ylabel("log10($\epsilon_i$)")
 plt.plot(np.log10(h), epsilon)
-plt.show()
+plt.savefig("relative_error.png")
